5d642e19c9561b41ed14a99b6ab20aff38e1b6f1
shaozhe <shaozhecheng@outlook.com>
2019-10-10 13:31:34 +0800

    add analysis

--- src/experiment.py
+++ src/experiment.py
@@ -1,23 +1,26 @@
 import numpy as np
+
+
 class Experiment():
-    def __init__(self, trial, writer, experimentValues,darwImage,restImage,darwBackground,hasRest=True):
+    def __init__(self, trial, writer, experimentValues, darwImage, restImage, darwBackground, hasRest=True):
         self.trial = trial
         self.writer = writer
         self.experimentValues = experimentValues
-        self.darwImage=darwImage
-        self.restImage=restImage
-        self.darwBackground=darwBackground
-        self.hasRest=hasRest
-    def __call__(self, designValues,restDuration):
-        for trialIndex in range(len(designValues)):          
+        self.darwImage = darwImage
+        self.restImage = restImage
+        self.darwBackground = darwBackground
+        self.hasRest = hasRest
+
+    def __call__(self, designValues, restDuration):
+        for trialIndex in range(len(designValues)):
             condition = designValues[trialIndex]
             results = self.trial(condition)
 
-            results["trail"] = trialIndex + 1                      
+            results["trail"] = trialIndex + 1
             response = self.experimentValues.copy()
             response.update(results)
             self.writer(response, trialIndex)
-            if np.mod(trialIndex+1,restDuration)==0 & self.hasRest:
+            if np.mod(trialIndex + 1, restDuration) == 0 & self.hasRest:
                 self.darwBackground()
-                self.darwImage(self.restImage)  
+                self.darwImage(self.restImage)
         return results
--- tests/testTrial.py
+++ tests/testTrial.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import pygame
-from src.visualization import DrawState, DrawImage, DrawBackground, DrawImageClick,DrawText
+from src.visualization import DrawState, DrawImage, DrawBackground, DrawImageClick, DrawText
 from src.design import samplePosition
 from src.trial import ChaseTrial, CheckHumanResponse
 import random
@@ -18,8 +18,8 @@ if __name__ == '__main__':
     circleSize = 10
     imageHeight = 80
     lineWidth = 1
-    xBoundary = [leaveEdgeSpace, screenWidth-leaveEdgeSpace*2]
-    yBoundary = [leaveEdgeSpace, screenHeight-leaveEdgeSpace*2]
+    xBoundary = [leaveEdgeSpace, screenWidth - leaveEdgeSpace * 2]
+    yBoundary = [leaveEdgeSpace, screenHeight - leaveEdgeSpace * 2]
     numOfAgent = 4
 
     screenColor = THECOLORS['black']
@@ -30,8 +30,8 @@ if __name__ == '__main__':
 
     drawImage = DrawImage(screen, imageHeight)
     drawText = DrawText(screen)
-    drawImageClick = DrawImageClick(screen, imageHeight, circleColorList,drawText)
-    drawBackground = DrawBackground(screen, xBoundary,yBoundary,lineColor,lineWidth)
+    drawImageClick = DrawImageClick(screen, imageHeight, circleColorList, drawText)
+    drawBackground = DrawBackground(screen, xBoundary, yBoundary, lineColor, lineWidth)
     drawState = DrawState(drawBackground, numOfAgent, screen, screenColor, circleColorList, circleSize)
 
     picturePath = os.path.join(os.path.abspath(os.path.join(os.getcwd(), os.pardir)), 'pictures')
@@ -39,12 +39,12 @@ if __name__ == '__main__':
     keysForCheck = {'f': 0, 'j': 1}
     clickWolfImage = pygame.image.load(os.path.join(picturePath, 'clickwolf.png'))
     clickSheepImage = pygame.image.load(os.path.join(picturePath, 'clicksheep.png'))
-    checkHumanResponse = CheckHumanResponse(keysForCheck, drawImageClick, clickWolfImage,clickSheepImage)
+    checkHumanResponse = CheckHumanResponse(keysForCheck, drawImageClick, clickWolfImage, clickSheepImage)
     displayFrames = 10
 
-    stimulusXBoundary = [xBoundary[0]+circleSize, xBoundary[1]-circleSize]
-    stimulusYBoundary = [yBoundary[0]+circleSize, yBoundary[1]-circleSize]
-    sampleTrajectory = lambda numOfAgent, displayFrames: [[samplePosition(stimulusXBoundary, stimulusYBoundary) for i in range(numOfAgent)]for j in range(displayFrames)]
+    stimulusXBoundary = [xBoundary[0] + circleSize, xBoundary[1] - circleSize]
+    stimulusYBoundary = [yBoundary[0] + circleSize, yBoundary[1] - circleSize]
+    sampleTrajectory = lambda numOfAgent, displayFrames: [[samplePosition(stimulusXBoundary, stimulusYBoundary) for i in range(numOfAgent)] for j in range(displayFrames)]
 
     stimulus = {1: sampleTrajectory(numOfAgent, displayFrames),
                 2: sampleTrajectory(numOfAgent, displayFrames),
@@ -52,7 +52,7 @@ if __name__ == '__main__':
                 4: sampleTrajectory(numOfAgent, displayFrames)}
 
     condition = [1, 2, 3, 4]
-    trial = ChaseTrial(displayFrames, drawState,drawImage, stimulus, checkHumanResponse, clickWolfImage)
+    trial = ChaseTrial(displayFrames, drawState, drawImage, stimulus, checkHumanResponse, clickWolfImage)
     randomCondition = random.choice(condition)
     results = trial(randomCondition)
     print(results)
